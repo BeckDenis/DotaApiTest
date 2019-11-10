a9ddd2a1880464e8e8c45ba96935c3c7b089670e
Denis <denisbeck555@gmail.com>
2019-11-10 10:54:17 +0600

    redesigned

--- app/src/main/java/com/example/dotaapitest/network/DotaProperty.kt
+++ app/src/main/java/com/example/dotaapitest/network/DotaProperty.kt
@@ -14,7 +14,12 @@ data class DotaProperty(
     val attack_type: String,
     val base_health: Int,
     val base_mana: Int,
-    val base_attack_min: Int
+    val base_attack_min: Int,
+    val base_attack_max: Int,
+    val roles: List<String>,
+    val move_speed: Int,
+    val attack_range: Int,
+    val legs: Int
 ) : Parcelable {
     val isAttackType
         get() = attack_type == "Melee"
@@ -25,6 +30,18 @@ data class DotaProperty(
     val baseMana = base_mana.toString()
     @IgnoredOnParcel
     val baseAttackMin = base_attack_min.toString()
+    @IgnoredOnParcel
+    val baseAttackMax = base_attack_max.toString()
+    @IgnoredOnParcel
+    val rolesString = roles.toString()
+        .replace("[","")
+        .replace("]","")
+    @IgnoredOnParcel
+    val moveSpeed = move_speed.toString()
+    @IgnoredOnParcel
+    val attackRange = attack_range.toString()
+    @IgnoredOnParcel
+    val legsString = legs.toString()
 }
 
 
